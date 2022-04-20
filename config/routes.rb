e80bbf7ca85952df0a325cbf03e9b78c435f622c
John Hennerich <john.hennerich@gmail.com>
2022-04-20 17:04:27 -0600

    remove / from route for heroku deploy

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :invoices, controller: 'merchant_invoices', only: [:index, :show, :update]
   end
 
-  get '/', to: 'admin#index'
+#  get '/', to: 'admin#index'
 
   get '/admin', to: 'admin#index'
 
