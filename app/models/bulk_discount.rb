81c6db368bb922feb5a27010e5a16e53410eff91
John Hennerich <john.hennerich@gmail.com>
2022-04-22 15:04:47 -0600

    Feat: bulkdiscount setup

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -2,4 +2,5 @@ class BulkDiscount < ApplicationRecord
   validates_presence_of :percentage,
                         :quantity_threshold
   belongs_to :merchant
+  has_many :items, through: :merchants
 end
