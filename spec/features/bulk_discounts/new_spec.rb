db95e73a2779822cc774d3bdafca9f6eba08d1f6
John Hennerich <john.hennerich@gmail.com>
2022-04-23 11:37:20 -0600

    Test: spec test for new bulk discount

---
+++ spec/features/bulk_discounts/new_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe "Applications new page" do
+  before :each do
+    @merch = Merchant.create(name:"StarWars R Us")
+    @bulk_discount = BulkDiscount.create(name: "DiscountA", percentage: 10, quantity_threshold: 10)
+  end
+
+  it "has form to create new application" do
+    visit new_merchant_bulk_discount_path(@merch)
+    fill_in("Name", with: 'May the Fourth')
+    fill_in('percentage', with: 10)
+    fill_in('quantity_threshold', with: 10)
+
+    click_button("Submit")
+    expect(current_path).to eq( merchant_bulk_discounts_path(@merch))
+    expect(page).to have_content("May the Fourth")
+  end
+end
